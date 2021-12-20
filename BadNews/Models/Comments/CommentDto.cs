1acb5a0dd22d71f395fcef688f0ef6de29d96cdc
Darya <dashav1605@mail.ru>
2021-12-20 18:03:31 +0500

    All tasks done

---
+++ BadNews/Models/Comments/CommentDto.cs
@@ -0,0 +1,15 @@
+﻿namespace BadNews.Models.Comments
+{
+    public class CommentDto
+    {
+        public string User { get; set; }
+        
+        public string Value { get; set; }
+
+        public CommentDto(string user, string value)
+        {
+            User = user;
+            Value = value;
+        }
+    }
+}
---
+++ BadNews/Models/Comments/CommentsDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BadNews.Repositories.Comments;
+
+namespace BadNews.Models.Comments
+{
+    public class CommentsDto
+    {
+        public Guid NewsId { get; set; }
+
+        public IReadOnlyCollection<CommentDto> Comments { get; set; }
+
+        public CommentsDto(IReadOnlyCollection<Comment> comments)
+        {
+            Comments = comments.Select(c => new CommentDto(c.User, c.Value)).ToArray();
+        }
+    }
+}
